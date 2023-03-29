9e62a68943181bd5338f16764ff40751b8a9669d
Sparsh Verma <2020eeb1210@iitrpr.ac.in>
2023-03-29 11:25:30 +0530

    separate front end folder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import MainRouter from './components/MainRouter';
+import {BrowserRouter} from 'react-router-dom'
+import { ThemeProvider } from '@material-ui/styles'
+import theme from './components/theme';
+import React from 'react';
 
 function App() {
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles)
+    }
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <MainRouter/>
+        </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
